32868f21717d1c5c105cbf7bce50d3cbf4a44ac4
fanpengfei1 <fanpengfei1@jd.com>
2021-02-19 18:16:35 +0800

    fw

--- IRIS/route1.go
+++ IRIS/route1.go
@@ -6,8 +6,13 @@ func main() {
 	app := iris.New()
 
 	//handle method
-	app.Handle("GET", "/contact", func(ctx iris.Context) {
+	app.Handle("GET", "/name/{name:string}", func(ctx iris.Context) {
+		ctx.Application().Logger().Info("ctx.Params:", ctx.Params())
+
+		//TODO: ctx.Values
+		ctx.Application().Logger().Info("ctx.Values:", ctx.Values())
 		ctx.HTML("<h1> Hello from /contact </h1>")
 	})
 
+	app.Run(iris.Addr(":8080"))
 }
